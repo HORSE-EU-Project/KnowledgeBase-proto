fec1dc9837181290dd3278df370c3b04816fb0f7
unknown <alice.piemonti@mail.polimi.it>
2024-06-20 16:12:59 +0200

    added test

--- tests/test_guideline.py
+++ tests/test_guideline.py
@@ -5,6 +5,6 @@ sys.path.insert(0, dirname(abspath(__file__)))
 
 from src.guideline import add
 def test_audition():
-    assert add(1,2) == 3
+    assert add(1,1) == 2
     assert add(-1, 1) == 0
     assert add(10, -5) == 5
