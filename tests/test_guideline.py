cb6f59228289d63948b6bf45210cc469cc9dcdfb
unknown <alice.piemonti@mail.polimi.it>
2024-06-21 10:19:26 +0200

    fixed first test

--- tests/test_guideline.py
+++ tests/test_guideline.py
@@ -4,6 +4,7 @@ from os.path import abspath, dirname
 sys.path.insert(0, dirname(abspath(__file__)))
 
 from src.guideline import add
+
 def test_audition():
     assert add(1,1) == 2
     assert add(-1, 1) == 0
