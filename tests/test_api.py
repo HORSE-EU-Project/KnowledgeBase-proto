aa1259107eddb85cc3ffb7fbf6f8c9db50a71999
unknown <alice.piemonti@mail.polimi.it>
2024-06-27 15:09:13 +0200

    added api tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,24 +3,83 @@ from src.app.main import app
 
 client = TestClient(app)
 
-# vars
+# variables
 N_ATTACKS = 3       
 
 def test_hello_world():
-    response = client.get("/helloworld")
+
+    endpoint = "/helloworld"
+
+    # Test a generic GET to the server
+    response = client.get(endpoint)
+
+    # Assert that the response status code is 200 (OK)
     assert response.status_code == 200
+
+    # Assert that the request correctly returns an 'hello world
     assert response.json() == "hello world"
 
+
 def test_all_attacks():
-    response = client.get("/allattacks")
+
+    endpoint = "/allattacks"
+
+    # Test GET allattacks
+    response = client.get(endpoint)
+
+    # Assert that the response status code is 200 (OK)
     assert response.status_code == 200
+
+    # Assert that the response json contains the field 'attack_list'
     assert "attack_list" in response.json()
+
+    # Assert that the length of attacks returned is equal to the number of attacks expected in the db
     assert len(response.json()["attack_list"]) == N_ATTACKS
 
+
 def test_fetch_mitigations():
-    response = client.post("/mitigations", json={"attack_name": "pfcf_dos"})
+
+    endpoint = "/mitigations"
+    json = {
+        "attack_name": "pfcf_dos"
+        }
+
+    # Test POST to retrieve mitigations given 'pfcf_dos' as attack input
+    response = client.post(endpoint, json=json)
+
+    # Assert that the response status code is 200 (OK)
     assert response.status_code == 200
+
+    # Assert that the response json contains the fields 'attack_name' and 'mitiagions'
     assert all(key in response.json() for key in ["attack_name", "mitigations"])
+
+    # Assert that the response json mitigations is an object of length > 1
     assert len(response.json()["mitigations"]) > 1
+
+    # Assert that the first element of mitigations list is an object containing the fields 'name', 'priority', 'description'
     assert all(key in response.json()["mitigations"][0] for key in ["name", "priority", "description"])
-    # test errors here
+
+def test_fetch_mitigations_unknown_attack():
+    endpoint = "/mitigations"
+    json = {
+        "attack_name" : "unknown_attack_name"
+    }
+
+    # Test POST to retrieve mitigations given an unknown attack name
+    response = client.post(endpoint, json=json)
+
+    # Assert that the response status code is 404 (Not Found)
+    assert response.status_code == 404
+
+def test_fetch_mitigations_wrong_json():
+    endpoint = "/mitigations"
+    json = {
+        "no_attack_name" : "pfcf_dos"
+    }
+
+    # Test POST to retrieve mitigations with no attack_name field in the json body
+    response = client.post(endpoint, json=json)
+
+    # Assert that the response status code is 422 (Unprocessable Entity)
+    assert response.status_code == 422
+
--- tests/test_web_server.py
+++ tests/test_web_server.py
@@ -3,29 +3,41 @@ from os.path import abspath, dirname
 from sqlalchemy.exc import OperationalError
 from fastapi import HTTPException
 
-
-
 sys.path.insert(0, dirname(abspath(__file__)))
 
 from src.app.main import hello_world, get_all_attacks, get_mitigation
 
-## variables
+# variables
 N_ATTACKS_IN_DB = 3
 
 def test_hello_world():
     assert hello_world() == "hello world"
 
 def test_get_all_attacks():
+
+    # Assert that the function returns any response
     assert get_all_attacks() is not None
+
+    # Assert that the response has the n. of attacks expected from the db
     assert len(get_all_attacks().attack_list) == N_ATTACKS_IN_DB
+
+    # Assert that ntp_dos is one of the attacks in the db
     assert 'ntp_dos' in get_all_attacks().attack_list
 
 def test_get_mitigation():
+
+    # Assert that giving a wrong input, the function throws an HttpException
     try:
         get_mitigation('wrong_input')
         assert False
     except HTTPException:
         assert True
+
+    # Assert that giving a right input (ntp_dos) the function returns something
     assert get_mitigation('ntp_dos') is not None
+
+    # Assert that the function returns an object with length > 1
     assert len(get_mitigation('pfcf_dos')) > 1
+
+    # Assert that the name of the first mitigation to 'dns_reflection_amplification' is 'dns_service_disable' 
     assert get_mitigation('dns_reflection_amplification')[0].name == 'dns_service_disable'
